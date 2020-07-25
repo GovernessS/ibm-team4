417753a0416edb35b3e128153637744f339f39a1
Governess Simpson <35182441+GovernessS@users.noreply.github.com>
2020-07-24 20:57:03 -0500

    Revert "Update branch"

--- README.md
+++ README.md
@@ -17,10 +17,8 @@ Collaborating on Github: https://medium.com/@jonathanmines/the-ultimate-github-c
 
 ## Running the Application
 
-cd into my-app in the project directory and use:
-* To install dependencies.Please run this command in the my-app directory.  
-npm install 
-* To run the application
+cd into my-app in the project direcotry and use:
+
 npm start 
 
 .. or see live changes if you're using VS Code.
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,19 +1,8 @@
-
-
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Button, ButtonGroup } from 'reactstrap';
-import {Checkbox, Radio} from 'react-btn-checkbox';
-import ReactWidgetsForm from './ReactWidgetsForm'
-
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import { reducer as formReducer } from 'redux-form';
-
-
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,88 +10,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-ReactDOM.render(
-  <h1>Black Mental Health</h1>,
-  document.getElementById('root')
-);
-
-class QuoteMaker extends React.Component {
-  render() {
-    return (
-
-      <div>
-      <title>Black Mental Health</title>
-      <header className="Title_b">
-        <h1 className="Form-font Site-name"> Black Mental Health</h1>
-        <a className="Link" href="contact.html">
-          Contact Us
-        </a>
-        <a className="Link" href="about.html">
-          About
-        </a>
-      </header>
-      <div className="Text welcome">
-        <div className="welcome-text">
-          <h1>
-            Welcome to our website!
-            </h1>
-            <h2 id="texto">
-            We are here to assist you
-            </h2>
-
-
-        </div>
-      </div>
-      <div className="Text">
-        <p>
-
-            <div>
-            <ReactWidgetsForm />
-
-
-
-            </div>
-        </p>
-      </div>
-      <p className="Text">
-        Please enter your name here:
-        <br />
-       <input type="form" />
-      </p>
-      <button className="submit" type="submit">
-        Submit
-      </button>
-    </div>
-    );
-  }
-};
-
-const rootReducer = combineReducers({
-  form: formReducer,
-});
-
-const store = createStore(rootReducer);
-ReactDOM.render(
-  <Provider store={store}>
-    <QuoteMaker />
-  </Provider>,
-  document.getElementById('root')
-);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
