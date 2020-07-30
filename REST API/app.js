23f264ca5de8b038a6c15cb946b90b37a87ccf89
Twumasi Pennoh <twumasi.pennoh@gmail.com>
2020-07-29 23:54:45 -0400

    REST API

---
+++ REST API/app.js
@@ -0,0 +1,55 @@
+
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const CONNECTION_URL = "mongodb+srv://firafira:<password>@cluster0.swa4q.mongodb.net/BlackHelp?retryWrites=true&w=majority";
+const DATABASE_NAME = "testdb";
+
+var app = Express();
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+var database, collection;
+
+/**POST*/
+app.post("/connection1", (request, response) => {
+    collection.insert(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+
+/**GET*/
+app.get("/connection1", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+/**GET:ID*/
+app.get("/connection1/:id", (request, response) => {
+    collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+
+app.listen(5000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("personnel");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
