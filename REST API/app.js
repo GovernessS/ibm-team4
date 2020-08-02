21160ecc2d9075b84d2821c0e3d37d8b849e054c
Twumasi Pennoh <twumasi.pennoh@gmail.com>
2020-08-02 16:07:15 -0400

    Update app.js

--- REST API/app.js
+++ REST API/app.js
@@ -3,8 +3,10 @@ const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
-const CONNECTION_URL = "mongodb+srv://firafira:<password>@cluster0.swa4q.mongodb.net/BlackHelp?retryWrites=true&w=majority";
-const DATABASE_NAME = "testdb";
+//const CONNECTION_URL = "mongodb+srv://tpennoh:cUVw7Rb1Kyo0yecV@cluster0.vurao.mongodb.net/testdb?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://blackHelp2:6vCRbkpc1YA4UYln@cluster0.swa4q.mongodb.net/BlackHelp?retryWrites=true&w=majority";
+const DATABASE_NAME = "BlackHelp";
+//const DATABASE_NAME = "testdb";
 
 var app = Express();
 app.use(BodyParser.json());
@@ -12,7 +14,7 @@ app.use(BodyParser.urlencoded({ extended: true }));
 var database, collection;
 
 /**POST*/
-app.post("/connection1", (request, response) => {
+app.post("/Resources", (request, response) => {
     collection.insert(request.body, (error, result) => {
         if(error) {
             return response.status(500).send(error);
@@ -23,24 +25,36 @@ app.post("/connection1", (request, response) => {
 
 
 /**GET*/
-app.get("/connection1", (request, response) => {
-    collection.find({}).toArray((error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
+app.get("/Resources", (request, response) => {
+    if(Object.entries(request.query).length === 0){
+        collection.find({}).toArray((error, result) => {
+            if(error) {
+                return response.status(500).send(error);
+            }
+            response.send(result);
+        });
+    }
+    else{
+        collection.find(
+            {"specialty_1":request.query["specialty"]
+            }).toArray((error, result) => {
+            if(error) {
+                return response.status(500).send(error);
+            }
+            response.send(result);
+        });
+    }
 });
 
-/**GET:ID*/
-app.get("/connection1/:id", (request, response) => {
-    collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
-});
+// /**GET:ID*/
+// app.get("/Resources/:id", (request, response) => {
+//     collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+//         if(error) {
+//             return response.status(500).send(error);
+//         }
+//         response.send(result);
+//     });
+// });
 
 
 app.listen(5000, () => {
@@ -49,7 +63,7 @@ app.listen(5000, () => {
             throw error;
         }
         database = client.db(DATABASE_NAME);
-        collection = database.collection("personnel");
+        collection = database.collection("Resources");
         console.log("Connected to `" + DATABASE_NAME + "`!");
     });
 });
